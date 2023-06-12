177883f15602bcbc754d320e9f8430e00ac4290d
christopher-alden <christopheralden04@gmail.com>
2023-06-13 02:58:12 +0700

    try 2

--- src/components/sliders/ContentSlider.jsx
+++ src/components/sliders/ContentSlider.jsx
@@ -9,7 +9,7 @@ SwiperCore.use([Navigation, Pagination]);
 
 export default function ContentSlider({ title, courses, category }) {
   return (
-    <div className="w-full flex justify-center">
+    <div className="w-full flex justify-center no-x">
       <div className="flex-col w-full">
         <div className="text-2xl text-d-text font-bold mb-4 mx-auto pl-6 md:pl-12 lg:pl-16 xl:pl-40 lg:text-3xl md:text-2xl">{title}</div>
         {/* <div className="flex w-[1400px] mx-auto justify-between absolute translate-y-28">
